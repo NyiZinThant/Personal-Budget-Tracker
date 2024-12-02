a5969400979adee81b071ddd818a63b7f10689f2
NyiZinThant <nyizinthant@gmail.com>
2024-12-02 12:29:21 +0630

    add unit testing for currencyUtils

--- src/utils/currencyUtils.js
+++ src/utils/currencyUtils.js
@@ -1,17 +1,34 @@
 export const getFinancialSummary = function (data) {
+  if (!data) {
+    console.error("data can't be null");
+    return { totalSaving: 0, totalExpenses: 0, totalIncomes: 0 };
+  }
   let totalExpenses = 0;
   let totalIncomes = 0;
-  for (let el of data) {
+  data.forEach((el) => {
+    if (!el.type) return;
     if (el.type === 'Income') {
       totalIncomes += el.amount;
     } else {
       totalExpenses += el.amount;
     }
-  }
+  });
   let totalSaving = totalIncomes - totalExpenses;
   return { totalSaving, totalExpenses, totalIncomes };
 };
 
 export const calcPercentage = function (val, total) {
-  return ((val / total) * 100).toFixed(2);
+  if (!val) {
+    console.error("value can't be null");
+    return -1;
+  }
+  if (!total) {
+    console.error("total can't be null");
+    return -1;
+  }
+  if (val > total) {
+    console.error("val can't be greater than total");
+    return -1;
+  }
+  return +((val / total) * 100).toFixed(2);
 };
---
+++ tests/utils/currencyUtils.test.js
@@ -0,0 +1,82 @@
+import { describe, expect, it, vi } from 'vitest';
+import {
+  calcPercentage,
+  getFinancialSummary,
+} from '../../src/utils/currencyUtils';
+
+describe('calcPercentage', () => {
+  it('should log error if value is null', () => {
+    const spyError = vi.spyOn(console, 'error');
+    calcPercentage(null, 100);
+    expect(spyError).toHaveBeenCalled();
+  });
+  it('should log error if total is null', () => {
+    const spyError = vi.spyOn(console, 'error');
+    calcPercentage(100);
+    expect(spyError).toHaveBeenCalled();
+  });
+  it('should log error if both total and value is null', () => {
+    const spyError = vi.spyOn(console, 'error');
+    calcPercentage();
+    expect(spyError).toHaveBeenCalled();
+  });
+  it('should log error if value is greater than total', () => {
+    const spyError = vi.spyOn(console, 'error');
+    calcPercentage(100, 10);
+    expect(spyError).toHaveBeenCalled();
+  });
+  it('should return percentage', () => {
+    expect(calcPercentage(10, 100)).toBe(10);
+  });
+});
+describe('getFinancialSummary', () => {
+  it('should log error if data is empty', () => {
+    const spyError = vi.spyOn(console, 'error');
+    expect(getFinancialSummary()).toEqual({
+      totalSaving: 0,
+      totalExpenses: 0,
+      totalIncomes: 0,
+    });
+    expect(spyError).toHaveBeenCalled();
+  });
+  it('should return empty values summary if data is invalid', () => {
+    expect(getFinancialSummary([])).toEqual({
+      totalSaving: 0,
+      totalExpenses: 0,
+      totalIncomes: 0,
+    });
+  });
+  it('should return financial summary', () => {
+    const data = [
+      {
+        id: 1,
+        date: '2024-12-01',
+        description: 'Grocery shopping',
+        category: 'Food',
+        type: 'Expense',
+        amount: 50.75,
+      },
+      {
+        id: 2,
+        date: '2024-12-01',
+        description: 'Salary',
+        category: 'Income',
+        type: 'Income',
+        amount: 1500.0,
+      },
+      {
+        id: 3,
+        date: '2024-12-02',
+        description: 'Electricity bill',
+        category: 'Utilities',
+        type: 'Expense',
+        amount: 120.5,
+      },
+    ];
+    expect(getFinancialSummary(data)).toEqual({
+      totalSaving: 1328.75,
+      totalExpenses: 171.25,
+      totalIncomes: 1500.0,
+    });
+  });
+});
